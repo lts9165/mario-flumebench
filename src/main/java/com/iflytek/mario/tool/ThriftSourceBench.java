1c5ba54dc433cb6220afefda07667588cc0911ec
unknown <tsli@tsli.iflytek.com>
2014-06-04 09:11:52 +0800

    1.0-beta

--- src/main/java/com/iflytek/mario/tool/ThriftSourceBench.java
+++ src/main/java/com/iflytek/mario/tool/ThriftSourceBench.java
@@ -2,71 +2,126 @@ package com.iflytek.mario.tool;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ThriftSourceBench implements Tool {
-  public static Options opt = new Options();
-  private static String flumeIp;
-  private static int flumePort;
-  private static int eventSize;
-  private static int threadsNum;
-  private static int eventNum;
-  private static int batchSize;
-  public ThriftSourceBench() {
-    opt.addOption("h", "help", false, "Print help for this bench tool");
-    opt.addOption("t", "threads", true, "The num of bench threads");
-    opt.addOption("s", "size", true, "The data size of event");
-    opt.addOption("n", "num", true, "The num of events each thread send");
-    opt.addOption("i", "ip", true, "Flume ip");
-    opt.addOption("p", "port", true, "Flume port");
-    opt.addOption("b", "batchsize", true, "Batch Size");
-  }
-  
-  @Override
-  public int run(InputStream in, PrintStream out, PrintStream err,
-      List<String> args) {
-    try {
-      BasicParser parser = new BasicParser();
-      String[] argss = new String[args.size()];
-      CommandLine cl = parser.parse(opt, args.toArray(argss));
-      if (cl.hasOption('h')) {
-        HelpFormatter f = new HelpFormatter();
-        f.printHelp("thriftsource", opt);
-      } else {
-        flumeIp = cl.getOptionValue("i");
-        flumePort = Integer.parseInt(cl.getOptionValue("p"));
-        eventSize = Integer.parseInt(cl.getOptionValue("s"));
-        threadsNum = Integer.parseInt(cl.getOptionValue("t"));
-        eventNum = Integer.parseInt(cl.getOptionValue("n"));
-        if (cl.getOptionValue("b") == null)
-          batchSize = 1000;
-        else
-          batchSize = Integer.parseInt(cl.getOptionValue("b"));
-      }
-    } catch (ParseException e) {
-      HelpFormatter f = new HelpFormatter();
-      f.printHelp("thriftsource", opt);
-      return 0;
-    }
-  
-    
-    return 0;
-  }
-  
-  @Override
-  public String getName() {
-    return "thriftsource";
-  }
-  
-  @Override
-  public String getShortDescription() {
-    return "flume thrift source bench";
-  }
-  
+	private static final Logger logger = LoggerFactory
+			.getLogger(ThriftSourceBench.class);
+	public static Options opt = new Options();
+	private static String flumeIp;
+	private static int flumePort;
+	private static int eventSize;
+	private static int threadsNum;
+	private static int eventNum;
+	private static int batchSize;
+	private ArrayList<ThriftSourceBenchTask> allTasks;
+	private ExecutorService executor;
+
+	public ThriftSourceBench() {
+		opt.addOption("h", "help", false, "Print help for this bench tool");
+		opt.addOption("t", "threads", true, "The num of bench threads");
+		opt.addOption("s", "size", true, "The data size of event");
+		opt.addOption("n", "num", true, "The num of events each thread send");
+		opt.addOption("i", "ip", true, "Flume ip");
+		opt.addOption("p", "port", true, "Flume port");
+		opt.addOption("b", "batchsize", true, "Batch Size");
+	}
+
+	@Override
+	public int run(InputStream in, PrintStream out, PrintStream err,
+			List<String> args) {
+		try {
+			BasicParser parser = new BasicParser();
+			String[] argss = new String[args.size()];
+			CommandLine cl = parser.parse(opt, args.toArray(argss));
+			if (cl.hasOption('h')) {
+				HelpFormatter f = new HelpFormatter();
+				f.printHelp("thriftsource", opt);
+				return 0;
+			} else {
+				flumeIp = cl.getOptionValue("i");
+				flumePort = Integer.parseInt(cl.getOptionValue("p"));
+				eventSize = Integer.parseInt(cl.getOptionValue("s"));
+				threadsNum = Integer.parseInt(cl.getOptionValue("t"));
+				eventNum = Integer.parseInt(cl.getOptionValue("n"));
+				if (cl.getOptionValue("b") == null)
+					batchSize = 1000;
+				else
+					batchSize = Integer.parseInt(cl.getOptionValue("b"));
+
+			}
+		} catch (ParseException e) {
+			HelpFormatter f = new HelpFormatter();
+			f.printHelp("thriftsource", opt);
+			return 0;
+		}
+
+		executor = Executors.newFixedThreadPool(threadsNum);
+		allTasks = new ArrayList(threadsNum);
+		for (int i = 0; i < threadsNum; ++i) {
+			ThriftSourceBenchTask benchTask;
+			try {
+				benchTask = new ThriftSourceBenchTask(flumeIp, flumePort,
+						batchSize, eventSize, eventNum);
+				allTasks.add(benchTask);
+				executor.execute(benchTask);
+			} catch (TTransportException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		executor.shutdown();
+		try {
+			while (!executor.awaitTermination(5, TimeUnit.SECONDS))
+				;
+			executor.shutdown();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return 0;
+	}
+
+	public void fini() {
+		if (executor != null) {
+			for (ThriftSourceBenchTask task : allTasks) {
+				task.stop();
+			}
+			executor.shutdown();
+			try {
+				if (executor.awaitTermination(5, TimeUnit.SECONDS)) {
+					executor.shutdownNow();
+				}
+			} catch (Exception e) {
+				logger.error(
+						"Interrupted while waiting for transfer threads executor to shut down:{}",
+						e.getMessage());
+			}
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "thriftsource";
+	}
+
+	@Override
+	public String getShortDescription() {
+		return "flume thrift source bench";
+	}
+
 }
--- src/main/java/com/iflytek/mario/tool/ThriftSourceBenchTask.java
+++ src/main/java/com/iflytek/mario/tool/ThriftSourceBenchTask.java
@@ -1,7 +1,10 @@
 package com.iflytek.mario.tool;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.flume.event.EventBuilder;
 import org.apache.flume.thrift.ThriftFlumeEvent;
 import org.apache.flume.thrift.ThriftSourceProtocol;
 import org.apache.thrift.TException;
@@ -13,67 +16,76 @@ import org.apache.thrift.transport.TTransportException;
 import org.slf4j.*;
 
 public class ThriftSourceBenchTask implements Runnable {
-  private static final Logger logger = LoggerFactory
-      .getLogger(ThriftSourceBench.class);
-  private TTransport transport;
-  private ThriftSourceProtocol.Client client;
-  
-  boolean sleep = false;
-  boolean stop = false;
-  
-  ArrayList<ThriftFlumeEvent> events;
-  
-  public ThriftSourceBenchTask(String host, int port, int batchSize,
-      int eventSize, int eventNum) throws TTransportException {
-    transport = new TFramedTransport(new TSocket(host, port));
-    client = new ThriftSourceProtocol.Client(new TCompactProtocol(transport));
-    events = new ArrayList<ThriftFlumeEvent>();
-    ThriftFlumeEvent event = new ThriftFlumeEvent();
-    
-    for (int i = 0; i < batchSize; ++i) {
-      
-    }
-    try {
-      transport.open();
-    } catch (TTransportException e) {
-      logger.error("Open thrift transport error:{}", e.getMessage());
-      transport.close();
-      throw e;
-    }
-  }
-  
-  @Override
-  public void run() {
-    while (!stop) {
-      
-      // fetch log from queue.
-      ThriftFlumeEvent event = null;
-      if (null != event) {
-        events.add(event);
-      }
-      
-      // send
-      try {
-        if (!transport.isOpen()) {
-          transport.open();
-        }
-        if (events.size() > 0) {
-          client.appendBatch(events);
-          events.clear();
-        }
-        
-      } catch (TException e1) {
-        e1.printStackTrace();
-        logger.error("Thread [{}] append batch error:{}", Thread
-            .currentThread().getId(), e1.getMessage());
-        transport.close();
-      }
-      
-    }
-  }
-  
-  private void stop() {
-    stop = true;
-    transport.close();
-  }
+	private static final Logger logger = LoggerFactory
+			.getLogger(ThriftSourceBenchTask.class);
+	private TTransport transport;
+	private ThriftSourceProtocol.Client client;
+
+	private boolean stop = false;
+
+	ArrayList<ThriftFlumeEvent> events;
+	private int maxEvents;
+
+	public ThriftSourceBenchTask(String host, int port, int batchSize,
+			int eventSize, int eventNum) throws TTransportException {
+		transport = new TFramedTransport(new TSocket(host, port));
+		client = new ThriftSourceProtocol.Client(
+				new TCompactProtocol(transport));
+		events = new ArrayList<ThriftFlumeEvent>();
+		maxEvents = eventNum;
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("s.n", "flume_source_bench");
+		headers.put("timestamp", String.valueOf(System.currentTimeMillis()));
+		ThriftFlumeEvent event = new ThriftFlumeEvent();
+		event.setBody(new byte[eventSize]);
+		event.setHeaders(headers);
+		for (int i = 0; i < batchSize; ++i) {
+			events.add(event);
+		}
+		try {
+			transport.open();
+		} catch (TTransportException e) {
+			logger.error("Open thrift transport error:{}", e.getMessage());
+			transport.close();
+			throw e;
+		}
+	}
+
+	@Override
+	public void run() {
+		int eventNum = 0;
+		int errorNum = 0;
+		long startTime, stopTime;
+
+		startTime = System.currentTimeMillis();
+		while (!stop) {
+			// send
+			try {
+				if (!transport.isOpen()) {
+					transport.open();
+				}
+				client.appendBatch(events);
+				eventNum += events.size();
+				if (eventNum >= maxEvents) {
+					stop();
+				}
+			} catch (TException e1) {
+				logger.error("Thread [{}] append batch error:{}", Thread
+						.currentThread().getId(), e1.getMessage());
+				errorNum++;
+				transport.close();
+			}
+
+		}
+		stopTime = System.currentTimeMillis();
+		double costSec = (stopTime - startTime) / 1000.0;
+		System.out.println("Thread" + Thread.currentThread().getId()
+				+ " stop, time cost: " + costSec + " TPS: " + (eventNum - errorNum)
+				/ costSec + " Errors: " + errorNum);
+	}
+
+	void stop() {
+		stop = true;
+		transport.close();
+	}
 }
